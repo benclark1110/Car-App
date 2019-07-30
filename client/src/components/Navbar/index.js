08b5d1c0af8aec7f24fd4035b04e3005ad3be123
benclark1110 <benclark1110@gmail.com>
2019-07-29 21:15:26 -0400

    google sign in almost complete

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,44 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 import GoogleLogin from 'react-google-login';
+import GoogleLogout from 'react-google-login';
 import { Link } from "react-router-dom";
 
-function Nav() {
-  const responseGoogle = (response) => {
-    console.log(response);
+class Nav extends Component {
+  
+  state = {
+    loggedIn: false,
+    givenName: "",
+    familyName: "",
+    email: "",
+    image: ""
+  };
+  
+   render() {
+     
+    const responseGoogle = (response) => {
+      this.setState({
+        loggedIn: true,
+        givenName: response.profileObj.givenName,
+        familyName: response.profileObj.familyName,
+        email: response.profileObj.email,
+        image: response.profileObj.imageUrl
+      });
+      console.log(this.state)
+    };
+
+    const logout = (response) => {
+      console.log(response);
+      this.setState({
+        loggedIn: false,
+        givenName: "",
+        familyName: "",
+        email: "",
+        image: ""
+      });
+      console.log(this.state)
+    };
+
+    let authButton;
+      if (!this.state.loggedIn) {
+        authButton = <GoogleLogin
+        clientId="321790988998-dd7ojo9jl0oaqn9nhltn85g798gv53ve.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      } else {
+        authButton = <GoogleLogout
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      >
+      </GoogleLogout>
+      };
+
+    return (
+      <div>
+          <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+              <a className="navbar-brand">Fixed navbar</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                    <a className="nav-link">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link">Link</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+              </div>
+              <Link to={"/home"}>
+                {authButton}
+              </Link>
+          </nav>
+        </div>
+    );
   }
-  return (
-    <div>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a className="navbar-brand">Fixed navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav mr-auto">
-                  <li className="nav-item active">
-                  <a className="nav-link">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li className="nav-item">
-                  <a className="nav-link">Link</a>
-                  </li>
-                  <li className="nav-item">
-                  <a className="nav-link disabled">Disabled</a>
-                  </li>
-              </ul>
-            </div>
-            <Link to={"/home"}>
-            <GoogleLogin
-              clientId="321790988998-dd7ojo9jl0oaqn9nhltn85g798gv53ve.apps.googleusercontent.com"
-              buttonText="Login"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={'single_host_origin'}
-            />
-            </Link>
-        </nav>
-      </div>
-  );
 }
 
 export default Nav;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron/index";
+import Nav from "../components/Navbar/index";
+import Footer from "../components/Footer/index";
 
 class Home extends Component {
   // componentDidMount() {
@@ -9,7 +11,9 @@ class Home extends Component {
     render() {
       return (
         <div>
-            <Jumbotron />
+          <Nav />
+          <Jumbotron />
+          <Footer />
         </div>
       );
     }
